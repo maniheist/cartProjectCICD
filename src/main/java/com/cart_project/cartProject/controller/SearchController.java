54a0de8926210a64c11245575f418a4a278c2ce2
Mani Sankar Reddy Kalluri <manisankarr+Zeta@zeta.tech>
2025-03-21 18:23:57 +0530

    changing controller

--- src/main/java/com/cart_project/cartProject/controller/SearchController.java
+++ src/main/java/com/cart_project/cartProject/controller/SearchController.java
@@ -30,7 +30,7 @@ public class SearchController {
         }
     }
 
-    @GetMapping(value = "/{userId}/startsWith/{productName}")
+    @GetMapping(value = "/{userId}/startWith/{productName}")
     public ResponseEntity<List<Product>> allProductsThatStartWith(@PathVariable int userId,@PathVariable String productName){
         return ResponseEntity.ok(searchService.allProductsThatStartWith(userId,productName));
     }
